6e786a41029cebb52aa6e75cd49e389196f7da9f
rileyxion1203 <rileyxiong1203@gmail.com>
2024-10-01 16:41:10 -0400

    Update practice.rb

--- practice.rb
+++ practice.rb
@@ -1,2 +1,15 @@
-# Write code that prints "Hello World"
+#0.Write a while loop that prints the word "hello" 5 times.
+number = 0
+while number<5
+    puts "Hello"
+    number = number + 1
+end
+
+
+
+# 1.Write code that prints "Hello World"
 puts "Hello World"
+
+#2.Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+word = "morning!"
+puts word
