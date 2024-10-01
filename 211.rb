a22ca502feb23e10506a0467a1f7f0c5091c1bfe
rileyxion1203 <rileyxiong1203@gmail.com>
2024-10-01 18:18:58 -0400

    Create 211.rb

---
+++ 211.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a while loop that prints the numbers 50 to 70.
+number = 50
+while number < 71
+    puts number 
+    number = number + 1
+end
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I will practice array tomorrow. 
