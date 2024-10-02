a12d7438e724df49b36c04fe1a26beddd0e557e3
rileyxion1203 <rileyxiong1203@gmail.com>
2024-10-01 21:09:18 -0400

    Add exercise solutions

--- 211.rb
+++ 211.rb
@@ -14,4 +14,4 @@ end
 # If so, describe the next skill you will start to practice tomorrow.
 # If not, describe the part you got stuck on that requires more practice.
 
-# I will practice array tomorrow. 
+puts "I will practice array tomorrow."
