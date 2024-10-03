8a2100ad4e1fb176e161d3f6975d66b6fd9da598
rileyxion1203 <rileyxiong1203@gmail.com>
2024-10-03 16:19:34 -0400

    Add exercise solutions

---
+++ 241.rb
@@ -0,0 +1,6 @@
+# Create an array to store 3 strings with lower case letters. 
+# Then change the third string to have all capital letters and print the array on one line.
+
+items = ["my", "name", "is"]
+items[2] = items[2].upcase
+pp items
