f5104d8f711d0d26b89d8a26af3890645cc10898
rileyxion1203 <rileyxiong1203@gmail.com>
2024-10-03 15:16:32 -0400

    Add exercise solutions

---
+++ 242.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+x = ["Hello,","today", "is"]
+puts x.push("Thursday!")
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+x = {"Hello" => "hello", "Name" => "Kevin"}
+x["Complement"]= "Nice job!"
+
+pp x
