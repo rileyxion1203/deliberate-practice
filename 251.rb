3e49ae9537b8d2a33667f4d69f2c0c811a591f29
rileyxion1203 <rileyxiong1203@gmail.com>
2024-10-05 16:55:19 -0400

    Add exercise solutions

---
+++ 251.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a method that takes in a number and returns the number times itself. 
+# Then run the method and print the result.
+
+def product(number)
+    return number*number 
+end
+
+pp product(6)
+
